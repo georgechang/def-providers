668368963254c40ee13dddd0a02472cd54cf72d9
George Chang <changg@gmail.com>
2020-02-08 11:18:23 -0500

    Initial commit

---
+++ src/GC.DataExchange.Providers.Rss/RssSettings.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.DataExchange;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC.DataExchange.Providers.Rss
+{
+    public class RssSettings : IPlugin
+    {
+        public RssSettings()
+        { 
+        }
+
+        public string Url { get; set; }
+    }
+}
